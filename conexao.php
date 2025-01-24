a7e9e977f7aeb14f72ad83554abd026bc9da5874
Henrique Santos <silvashrq@gmail.com>
2025-01-24 14:12:58 -0300

    Organizando a lógica e a semântica

--- conexao.php
+++ conexao.php
@@ -1,12 +1,20 @@
 <?php
 
+// Definir credenciais de conexão com o banco de dados
 $usuario = 'root';
 $senha = '';
 $database = 'memories';
 $host = 'localhost';
 
+// Criar a conexão com o banco de dados
 $mysqli = new mysqli($host, $usuario, $senha, $database);
 
-if ($mysqli->error) {
-    die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+// Verificar se ocorreu um erro na conexão
+if ($mysqli->connect_error) {
+    die("Falha ao conectar ao banco de dados: " . $mysqli->connect_error);
 }
+
+// Caso a conexão seja bem-sucedida
+echo "Conexão bem-sucedida!";
+
+?>
